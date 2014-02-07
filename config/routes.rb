d339266285aafc9a3c2f19c831974afe05d31b1e
Jesse Kevon <jessekevon@gmail.com>
2014-02-07 10:37:48 -0600

    Sign in page as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Banktel2::Application.routes.draw do
   devise_for :users
-  root :to => 'visitors#new'
+  # root :to => 'visitors#new'
+  # devise_for :users
+  root :to => redirect("/users/sign_in")   
+
+  # root "/users/sign_in" => "sessions#new"
 end
