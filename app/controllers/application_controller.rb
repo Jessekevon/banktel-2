e6d3a0bb2464ba3a321c3012097277bce56c0222
Jesse Kevon <jessekevon@gmail.com>
2014-02-20 12:10:57 -0600

    Add before_action :authenticate_user! to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # Only allow users signed in to acess the site.
+  before_action :authenticate_user! 
+
   def after_sign_in_path_for(user)
      "/pages/modules"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
+	  
+	  before_filter :authenticate_user!, :except => [:show, :index]
+
+	def index
+	  #rest of class
+	end
+
 
   def new
   end
