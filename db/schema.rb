a5055bc7c2dfb721c88f162ec48f43d26983393b
Jesse Kevon <jessekevon@gmail.com>
2014-03-28 16:25:39 -0500

    log out button hover state fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328184331) do
+ActiveRecord::Schema.define(version: 20140205165201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "messages", force: true do |t|
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "phone"
-    t.string   "string"
-    t.string   "email"
-    t.string   "subject"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "user_name"
