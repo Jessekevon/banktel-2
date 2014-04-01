decb75229aa0aeb4c837f48eaf27aabde487b8cf
Jesse Kevon <jessekevon@gmail.com>
2014-04-01 13:45:54 -0500

    Edit config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,24 @@ Banktel2::Application.configure do
   config.action_mailer.default_url_options = { :host => 'banktel.herokuapp.com' }
 Rails.application.routes.default_url_options[:host] = 'banktel.herokuapp.com'
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => ENV["GMAIL_DOMAIN"]
+  :user_name            => ENV["GMAIL_ACCOUNT"],
+  :password             => ENV["GMAIL_PASSWORD"],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+
+}
+
+
+
 
 
 
