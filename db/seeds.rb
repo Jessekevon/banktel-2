e6d85a86c03993dfea9c81ccb21a7ea6d30a3695
Jesse Kevon <jessekevon@gmail.com>
2014-04-03 11:37:30 -0500

    Add section content

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 User.create!([
-  { :email => "banktel@gmail.com",  :user_name => "Banktel", :password => "banktelbiz", :password_confirmation => "banktelbiz"},
+  { :email => "info@banktel.com",  :user_name => "Banktel", :password => "banktelbiz", :password_confirmation => "banktelbiz"},
 ])
