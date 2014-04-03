02cf1757ed766d6523eec165f5931e02ef80adbd
Jesse Kevon <jessekevon@gmail.com>
2014-04-02 20:32:02 -0500

    ENV variable test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,25 +83,9 @@ Rails.application.routes.default_url_options[:host] = 'banktel.herokuapp.com'
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 
-config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["GMAIL_DOMAIN"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
-
-# :address              => "smtp.gmail.com",
-# :port                 => 587,
-# :domain               => ENV["GMAIL_DOMAIN"]
-# :user_name            => ENV["GMAIL_ACCOUNT"],
-# :password             => ENV["GMAIL_PASSWORD"],
-# :authentication       => "plain",
-# :enable_starttls_auto => true
 
 }
 
